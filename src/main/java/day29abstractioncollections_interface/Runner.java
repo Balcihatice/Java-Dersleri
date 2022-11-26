3533909cd2d5b263b46be4a676d55fa7935abf5f
Balcihatice <aciceaybike@gmail.com>
2022-11-26 16:52:26 +0300

    Java Dersleri

--- src/main/java/day29abstractioncollections_interface/Runner.java
+++ src/main/java/day29abstractioncollections_interface/Runner.java
@@ -3,7 +3,7 @@ package day29abstractioncollections_interface;
 public class Runner {
 
     public static void main(String[] args) {
-
+       
 
         System.out.println(Engine.price);
         System.out.println(Ac.price);
